e4b61885c258cb75cba3a203073367282887ec93
huyThDEV <92528393+huykg1112@users.noreply.github.com>
2026-04-10 09:32:27 +0700

    Refactor code structure for improved readability and maintainability

--- tutorials/awesome_gallery/models/ir_action.py
+++ tutorials/awesome_gallery/models/ir_action.py
@@ -3,8 +3,8 @@ from odoo import fields, models
 
 
 class ActWindowView(models.Model):
-    _inherit = 'ir.actions.act_window.view'
+    _inherit = 'ir.actions.act_window.view' # kế thừa từ model ir.actions.act_window.view, để thêm một lựa chọn mới vào trường view_mode
 
-    view_mode = fields.Selection(selection_add=[
-        ('gallery', "Awesome Gallery")
-    ],  ondelete={'gallery': 'cascade'})
+    view_mode = fields.Selection(selection_add=[  # thêm lựa chọn mới vào trường view_mode
+        ('gallery', "Awesome Gallery") # thêm lựa chọn 'gallery' vào trường view_mode, với nhãn hiển thị là "Awesome Gallery"
+    ],  ondelete={'gallery': 'cascade'}) # thiết lập hành động khi xóa lựa chọn 'gallery', ở đây là 'cascade' có nghĩa là khi xóa lựa chọn 'gallery' thì sẽ xóa tất cả các bản ghi liên quan đến nó trong bảng ir.actions.act_window.view
