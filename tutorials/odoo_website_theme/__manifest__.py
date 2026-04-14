f1904290537b17e86f44f95ea071337e70826025
huyThDEV <92528393+huykg1112@users.noreply.github.com>
2026-04-14 17:06:36 +0700

    Add Odoo Website Theme assets and menu configuration
    - Introduced a new icon image for the theme located at static/description/icon.png.
    - Updated primary_variables.scss to append new color palettes and shapes, ensuring compatibility with existing variables.
    - Created odoo_theme_menus.xml to define a new menu item for the Odoo Website Theme, linking it to the corresponding action.

--- tutorials/odoo_website_theme/__manifest__.py
+++ tutorials/odoo_website_theme/__manifest__.py
@@ -1,23 +1,28 @@
 {
     'name': 'Odoo website Theme',
-    'category': 'Website/Theme',
     'depends': ['website'],
+    'version': '0.1',
+    'application': True,
+    'category': 'Tutorials',
+    'installable': True,
     'data': [
         # 'data/presets.xml',  # Color presets
+        'views/odoo_theme_menus.xml',
     ],
     'assets': {
         # Bundle 1: Primary variables (load TRƯỚC Bootstrap)
         'web._assets_primary_variables': [
-            'website_airproof/static/src/scss/primary_variables.scss',
+            'odoo_website_theme/static/src/scss/primary_variables.scss',
         ],
         # Bundle 2: Bootstrap overrides (load TRƯỚC Bootstrap, SAU primary_variables)
         'web._assets_frontend_helpers': [
-            ('prepend', 'website_airproof/static/src/scss/bootstrap_overridden.scss'),
+            ('prepend', 'odoo_website_theme/static/src/scss/bootstrap_overridden.scss'),
         ],
         # Bundle 3: Frontend assets (style thông thường)
         'web.assets_frontend': [
-            'website_airproof/static/src/scss/font.scss',
+            'odoo_website_theme/static/src/scss/font.scss',
             # Thêm SCSS khác...
         ],
     },
+    'license': 'AGPL-3'
 }
