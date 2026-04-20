92f0294899df5ea6e738a3df53f7450917a3db2e
huyThDEV <92528393+huykg1112@users.noreply.github.com>
2026-04-20 10:54:10 +0700

    feat: initialize pk_hoan_hao module with custom snippets, layout templates, and documentation

--- tutorials/pk_hoan_hao/__manifest__.py
+++ tutorials/pk_hoan_hao/__manifest__.py
@@ -2,6 +2,7 @@
     "name": "PK Hoan Hao Theme",
     "summary": "Website theme for PK Hoan Hao clinic",
     "category": "Website",
+    'sequence': 1,
     "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "author": "PK Hoan Hao",
@@ -9,6 +10,9 @@
     "data": [
         "views/hoan_hao_menu.xml",
         "views/layout/header.xml",
+        "views/layout/login.xml",
+        "views/snippets/snippets.xml",
+        "views/snippets/s_pk_badge.xml",
     ],
     "assets": {
         "web._assets_primary_variables": [
@@ -19,6 +23,8 @@
         ],
         "web.assets_frontend": [
             "pk_hoan_hao/static/src/scss/layout/header.scss",
+            "pk_hoan_hao/static/src/scss/snippets/s_pk_badge/000.scss",
+            "pk_hoan_hao/static/src/scss/page/login.scss",
         ],
     },
     "application": False,
